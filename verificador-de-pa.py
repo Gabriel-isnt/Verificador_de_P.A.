9f875f00ee7ed184f7093bcbf04b41e76ab8b713
At_oa <85297718+lulilolo@users.noreply.github.com>
2023-08-16 18:39:12 -0300

    verificador-de-pa.py

---
+++ verificador-de-pa.py
@@ -0,0 +1,91 @@
+# Verificar P.A.
+
+def erro(palavra) -> None:  # deixa qualquer texto em vermelho
+    """
+    Essa função retorna a palavra ou frase em vermelho
+
+    É usada para quando vou mostrar uma mensagem de erro qualquer
+    """
+    print(f'\033[91m{palavra}\033[0m')
+
+
+def tratando_sequencia() -> int:  # para não tratar no programa principal eu faço uma função
+    """
+    Essa função verifica e garante que o usuário digite um número positivo maior que 2 e retorna esse número
+    """
+    while True:
+        try:
+            a = int(input('>> ').strip())
+            if a > 2:
+                return a  # retornará o número se ele for maior que 2
+
+            else:
+                erro('Por favor, coloque números positivos maiores que 2')
+
+        except ValueError:
+            erro('Por favor, escreva somente números inteiros')
+
+
+def tratando_numero() -> int:
+    """
+    Essa função retorna o número colocado pelo usuário com tratamento de erro 
+    """
+
+    while True:
+        try:
+            return int(input('>> ').strip())
+
+        except ValueError:
+            erro('Por favor, escreva somente números inteiros')
+
+
+def verifica_pa(sequencia) -> str:
+    """
+    Essa função recebe uma lista e fala se os números dela são uma P.A.
+    """
+    
+    if isinstance(sequencia, list):
+
+        uau = sequencia[1] - sequencia[0]  # verifico a razão inicial da sequência
+
+        for index in range(len(sequencia) - 1):
+            if (sequencia[index + 1]  - sequencia[index]) != uau:
+                # razão inicial difere da nova razão não é uma P.A.
+                return 'não é uma P.A.'
+
+        return 'é uma P.A.'
+    
+    else:
+        return 'só posso analisar listas'
+
+
+# programa rodando
+while True:
+    numeros_usuario = []  # cria no início para não dar erro de escopo
+
+    print('O número que você digitar, será o tamanho da sequência.')
+    print('Qual tamanho você quer ?')
+    quantidade_sequencia = tratando_sequencia()  # pega o tamanho desejado da sequência
+
+    print('digite um número para a sequência,  ele não pode ser repetido')
+    termina = 0
+    while quantidade_sequencia != termina:  # garante que o usuário coloque todos os números na lista
+        sequencia_usuario = tratando_numero()
+
+        if sequencia_usuario not in numeros_usuario:
+
+            numeros_usuario.append(sequencia_usuario)  # adiciona o número se ele não for repetido
+            numeros_usuario.sort()  # ordena a lista
+
+            print(numeros_usuario)
+
+            termina += 1
+
+        else:  # se colocar um número repetido
+            erro('Não coloque números repetidos')
+
+
+    # verificação de P.A.
+    epa = verifica_pa(numeros_usuario)
+    print(epa)
+    break
